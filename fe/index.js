68e619a9bbd56725b90af6d45e92eb66637641c5
unknown <tedokojikagami@gmail.com>
2022-02-01 05:25:36 +0700

    i forgot to ignore lol

---
+++ fe/index.js
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './App'
+
+
+ReactDOM.hydrate(<App/>,  document.getElementById('root') )
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const React = require('react'); 
+const ReactDOMServer = require('react-dom/server');
+
+import App from '../fe/App'
+
+const app = express();
+const PORT = 3000;
+
+app.use('^/$', (req, res, next) => {
+    fs.readFile(path.resolve('./fe/index.html'), 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("fucked!");
+        }
+        return res.send(data.replace('<div id="root"></div>',
+        ` <div id="root">${ReactDOMServer.renderToString(<App/>)}</div>`));
+    })
+})
+
+
+app.listen(PORT, ()=>{
+console.log('fucking shit');
+})
