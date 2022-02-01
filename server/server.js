8904c2bfa8365c27e3064e52f60f40991bcb4160
unknown <tedokojikagami@gmail.com>
2022-02-01 11:14:47 +0700

    jest lib

--- server/server.js
+++ server/server.js
@@ -23,5 +23,5 @@ app.use('^/$', (req, res, next) => {
 
 
 app.listen(PORT, ()=>{
-console.log(`It's worked dude, listening at ${PORT}`);
+console.log(`It's worked dude, listening at http://localhost:${PORT}`);
 })
